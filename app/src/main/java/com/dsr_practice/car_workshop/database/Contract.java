6f81713e3df7ee6f4d624fbd91ef2b70cc65dce5
Margarita <margo.himera@yandex.ru>
2017-07-17 16:03:47 +0300

    Contract and database helper classes were added

---
+++ app/src/main/java/com/dsr_practice/car_workshop/database/Contract.java
@@ -0,0 +1,75 @@
+package com.dsr_practice.car_workshop.database;
+
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+// This class describes all local entities for Provider class
+public class Contract {
+    // To prevent someone from accidentally instantiating the contract class,
+    // give it an empty constructor.
+    public Contract() {}
+
+    static final String CONTENT_AUTHORITY = "com.dsr_practice.car_workshop.database";
+    static final Uri    BASE_CONTENT_URI  = Uri.parse("content://" + CONTENT_AUTHORITY);
+    private static final String PATH_MARKS  = "marks";
+    private static final String PATH_MODELS = "models";
+    private static final String PATH_JOBS   = "jobs";
+    private static final String SEPARATOR   = "/";
+
+    // Columns supported by "marks" records
+    public static class MarkEntry implements BaseColumns {
+        public static final String TABLE_NAME = "mark";
+        // Not to be confused with the database primary key, which is _ID
+        public static final String COLUMN_NAME_MARK_ID = "mark_id";
+        public static final String COLUMN_NAME_MARK_NAME = "mark_name";
+
+        // Fully qualified URI for "mark" resources
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_MARKS).build();
+        // MIME type for lists of marks
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + CONTENT_URI + SEPARATOR + PATH_MARKS;
+        // MIME type for individual marks
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + CONTENT_URI + SEPARATOR + PATH_MARKS;
+    }
+
+    // Columns supported by "models" records
+    public static class ModelEntry implements BaseColumns {
+        public static final String TABLE_NAME = "model";
+        // Not to be confused with the database primary key, which is _ID
+        public static final String COLUMN_NAME_MODEL_ID = "model_id";
+        public static final String COLUMN_NAME_MODEL_NAME = "model_name";
+        public static final String COLUMN_NAME_FK_MARK_ID = "fk_mark_id";
+
+        // Fully qualified URI for "mark" resources
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_MODELS).build();
+        // MIME type for lists of marks
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + CONTENT_URI + SEPARATOR + PATH_MODELS;
+        // MIME type for individual marks
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + CONTENT_URI + SEPARATOR + PATH_MODELS;
+    }
+
+    // Columns supported by "jobs" records
+    public static class JobEntry implements BaseColumns {
+        public static final String TABLE_NAME = "job";
+        // Not to be confused with the database primary key, which is _ID
+        public static final String COLUMN_NAME_JOB_ID = "job_id";
+        public static final String COLUMN_NAME_JOB_NAME = "job_name";
+        public static final String COLUMN_NAME_PRICE = "price";
+
+        // Fully qualified URI for "mark" resources
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_JOBS).build();
+        // MIME type for lists of marks
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + CONTENT_URI + SEPARATOR + PATH_JOBS;
+        // MIME type for individual marks
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + CONTENT_URI + SEPARATOR + PATH_JOBS;
+    }
+}
---
+++ app/src/main/java/com/dsr_practice/car_workshop/database/DatabaseHelper.java
@@ -0,0 +1,78 @@
+package com.dsr_practice.car_workshop.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "car_workshop.db";
+
+    private static final String TYPE_TEXT = " TEXT";
+    private static final String TYPE_INTEGER = " INTEGER";
+    private static final String PRIMARY_KEY = "INTEGER PRIMARY KEY";
+    private static final String COMMA_SEP = ",";
+
+    //region Create tables
+    private static final String SQL_CREATE_MARKS =
+            "CREATE TABLE " + Contract.MarkEntry.TABLE_NAME + " (" +
+                    Contract.MarkEntry._ID + PRIMARY_KEY + COMMA_SEP +
+                    Contract.MarkEntry.COLUMN_NAME_MARK_ID + TYPE_INTEGER + COMMA_SEP +
+                    Contract.MarkEntry.COLUMN_NAME_MARK_NAME + TYPE_TEXT + ")";
+
+    private static final String SQL_CREATE_MODELS =
+            "CREATE TABLE " + Contract.ModelEntry.TABLE_NAME + " (" +
+                    Contract.ModelEntry._ID + PRIMARY_KEY + COMMA_SEP +
+                    Contract.ModelEntry.COLUMN_NAME_MODEL_ID + TYPE_INTEGER + COMMA_SEP +
+                    Contract.ModelEntry.COLUMN_NAME_MODEL_NAME + TYPE_TEXT + COMMA_SEP +
+                    Contract.ModelEntry.COLUMN_NAME_FK_MARK_ID + TYPE_INTEGER + COMMA_SEP +
+                    "FOREIGN KEY(" + Contract.ModelEntry.COLUMN_NAME_FK_MARK_ID + ") REFERENCES" +
+                    Contract.MarkEntry.TABLE_NAME + "(" + Contract.MarkEntry.COLUMN_NAME_MARK_ID +
+                    ") ON DELETE CASCADE)";
+
+    private static final String SQL_CREATE_JOBS =
+            "CREATE TABLE " + Contract.JobEntry.TABLE_NAME + " (" +
+                    Contract.JobEntry._ID + PRIMARY_KEY + COMMA_SEP +
+                    Contract.JobEntry.COLUMN_NAME_JOB_ID + TYPE_INTEGER + COMMA_SEP +
+                    Contract.JobEntry.COLUMN_NAME_JOB_NAME + TYPE_TEXT + COMMA_SEP +
+                    Contract.JobEntry.COLUMN_NAME_PRICE + TYPE_INTEGER + ")";
+    //endregion
+
+    //region Drop tables
+    private static final String SQL_DROP_MARKS =
+            "DROP TABLE IF EXISTS " + Contract.MarkEntry.TABLE_NAME;
+    private static final String SQL_DROP_MODELS =
+            "DROP TABLE IF EXISTS " + Contract.ModelEntry.TABLE_NAME;
+    private static final String SQL_DROP_JOBS =
+            "DROP TABLE IF EXISTS " + Contract.JobEntry.TABLE_NAME;
+    //endregion
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_MARKS);
+        db.execSQL(SQL_CREATE_MODELS);
+        db.execSQL(SQL_CREATE_JOBS);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // This database is only a cache for online data, so its upgrade policy is
+        // to simply to discard the data and start over
+        db.execSQL(SQL_DROP_MARKS);
+        db.execSQL(SQL_DROP_JOBS);
+        onCreate(db);
+    }
+
+    @Override
+    public void onOpen(SQLiteDatabase db) {
+        super.onOpen(db);
+        if (!db.isReadOnly()) {
+            // Enable foreign key constraints
+            db.execSQL("PRAGMA foreign_keys=ON;");
+        }
+    }
+}
