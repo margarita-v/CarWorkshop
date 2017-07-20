54f8869355c1cebad7e2af082dace57402260f60
Margarita <margo.himera@yandex.ru>
2017-07-20 17:45:24 +0300

    SyncAdapter and SyncService were added

---
+++ app/src/main/java/com/dsr_practice/car_workshop/sync/SyncService.java
@@ -0,0 +1,27 @@
+package com.dsr_practice.car_workshop.sync;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+public class SyncService extends Service {
+    private static final Object LOCK = new Object();
+    private static SyncAdapter syncAdapter = null;
+
+    // Thread-safe constructor, creates static SyncAdapter instance.
+    @Override
+    public void onCreate() {
+        synchronized (LOCK) {
+            if (syncAdapter == null) {
+                syncAdapter = new SyncAdapter(getApplicationContext(), true);
+            }
+        }
+    }
+
+    // Return our SyncAdapter's IBinder
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
