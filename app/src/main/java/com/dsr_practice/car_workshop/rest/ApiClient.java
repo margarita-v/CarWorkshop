845eaefb5ae4f3778e5a504145efc9851b3b8959
Margarita <margo.himera@yandex.ru>
2017-07-21 20:57:04 +0300

    ApiClient was added and SyncAdapter was changed

---
+++ app/src/main/java/com/dsr_practice/car_workshop/rest/ApiClient.java
@@ -0,0 +1,22 @@
+package com.dsr_practice.car_workshop.rest;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient {
+    private static final String BASE_URL = "http://localhost:8000";
+
+    public static ApiInterface getApi() {
+        Gson gson = new GsonBuilder().create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        return retrofit.create(ApiInterface.class);
+    }
+}
--- app/src/main/java/com/dsr_practice/car_workshop/sync/SyncAdapter.java
+++ app/src/main/java/com/dsr_practice/car_workshop/sync/SyncAdapter.java
@@ -7,6 +7,19 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.content.SyncResult;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.dsr_practice.car_workshop.models.common.Job;
+import com.dsr_practice.car_workshop.models.common.Mark;
+import com.dsr_practice.car_workshop.models.common.Model;
+import com.dsr_practice.car_workshop.rest.ApiClient;
+import com.dsr_practice.car_workshop.rest.ApiInterface;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class SyncAdapter extends AbstractThreadedSyncAdapter {
     private static final String TAG = "SYNC_ADAPTER";
@@ -14,9 +27,12 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
     // Content resolver, for performing database operations
     private final ContentResolver contentResolver;
 
+    private static ApiInterface apiInterface;
+
     public SyncAdapter(Context context, boolean autoInitialize) {
         super(context, autoInitialize);
         contentResolver = context.getContentResolver();
+        apiInterface = ApiClient.getApi();
     }
 
     public SyncAdapter(Context context, boolean autoInitialize, boolean allowParallelSyncs) {
@@ -26,6 +42,57 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
 
     @Override
     public void onPerformSync(Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult) {
+        // Sync mark list
+        apiInterface.getMarks().enqueue(new Callback<List<Mark>>() {
+            @Override
+            public void onResponse(Call<List<Mark>> call, Response<List<Mark>> response) {
+                syncMarkList(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<List<Mark>> call, Throwable t) {
+                Toast.makeText(getContext(), "Fail to sync mark list with server", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Sync model list
+        apiInterface.getModels().enqueue(new Callback<List<Model>>() {
+            @Override
+            public void onResponse(Call<List<Model>> call, Response<List<Model>> response) {
+                syncModelList(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<List<Model>> call, Throwable t) {
+                Toast.makeText(getContext(), "Fail to sync model list with server", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Sync job list
+        apiInterface.getJobs().enqueue(new Callback<List<Job>>() {
+            @Override
+            public void onResponse(Call<List<Job>> call, Response<List<Job>> response) {
+                syncJobList(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<List<Job>> call, Throwable t) {
+                Toast.makeText(getContext(), "Fail to sync job list with server", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    //region Sync local database with server
+    private void syncMarkList(List<Mark> newList) {
 
     }
+
+    private void syncModelList(List<Model> newList) {
+
+    }
+
+    private void syncJobList(List<Job> newList) {
+
+    }
+    //endregion
 }
