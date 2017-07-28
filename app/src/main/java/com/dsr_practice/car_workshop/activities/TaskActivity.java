8ad842a081bd30f2998c350a488e179441336954
Margarita <margo.himera@yandex.ru>
2017-07-28 21:51:47 +0300

    Load marks info from database was added

--- app/src/main/java/com/dsr_practice/car_workshop/activities/TaskActivity.java
+++ app/src/main/java/com/dsr_practice/car_workshop/activities/TaskActivity.java
@@ -1,9 +1,13 @@
 package com.dsr_practice.car_workshop.activities;
 
 import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.SimpleCursorAdapter;
 import android.widget.Spinner;
@@ -15,6 +19,7 @@ public class TaskActivity extends AppCompatActivity {
 
     EditText etVIN, etNumber;
     Spinner spinnerMark, spinnerModel, spinnerWorks;
+    private ContentResolver contentResolver;
     private int[] viewsId = {android.R.id.text1};
 
     @Override
@@ -30,18 +35,24 @@ public class TaskActivity extends AppCompatActivity {
         spinnerWorks = (Spinner) findViewById(R.id.spinnerWorks);
 
         // Load data for spinners from database
-        ContentResolver contentResolver = getContentResolver();
-        Cursor cursor = contentResolver.query(Contract.JobEntry.CONTENT_URI,
-                Contract.JOB_NAMES_PROJECTION, null, null, null);
+        contentResolver = getContentResolver();
+
+        loadSpinnerInfo(Contract.MarkEntry.CONTENT_URI, Contract.MARK_NAMES_PROJECTION, spinnerMark);
+        //TODO Multiple choice for jobs
+        loadSpinnerInfo(Contract.JobEntry.CONTENT_URI, Contract.JOB_NAMES_PROJECTION, spinnerWorks);
+    }
+
+    private void loadSpinnerInfo(Uri uri, String[] projection, Spinner spinner) {
+        Cursor cursor = contentResolver.query(uri, projection, null, null, null);
         SimpleCursorAdapter adapter = new SimpleCursorAdapter(
                 this,
                 android.R.layout.simple_spinner_item,
                 cursor,
-                Contract.JOB_NAMES_PROJECTION,
+                projection,
                 viewsId,
                 0
         );
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerWorks.setAdapter(adapter);
+        spinner.setAdapter(adapter);
     }
 }
